181434002d429b4ed62b6d3d5e4419ae02535ba4
Ryan Whetten <whettenrmw@gmail.com>
2023-06-09 11:00:57 -0600

    add dino module

--- README.md
+++ README.md
@@ -5,8 +5,44 @@ A ReTiCo module for DINO. See below for more information.
 
 ### Example
 ```python
+import sys
+from retico import *
+
+prefix = '/path/to/modules/'
+sys.path.append(prefix+'retico-vision')
+sys.path.append(prefix+'retico-yolov8')
+sys.path.append(prefix+'retico-dino')
+
+from retico_vision.vision import WebcamModule 
+from retico_yolov8.yolov8 import Yolov8
+from retico_dino.dino import Dinov2ObjectFeatures
+
+
+
+webcam = WebcamModule()
+yolo = Yolov8()
+feats = Dinov2ObjectFeatures(show=True)
+debug = modules.DebugModule()
+
+
+webcam.subscribe(yolo)
+yolo.subscribe(feats)
+feats.subscribe(debug)
+
+run(webcam)
+
+print("Network is running")
+input()
+
+stop(webcam)
 ```
 
 Citation
 ```
+@misc{oquab2023dinov2,
+  title={DINOv2: Learning Robust Visual Features without Supervision},
+  author={Oquab, Maxime and Darcet, Timothée and Moutakanni, Theo and Vo, Huy V. and Szafraniec, Marc and Khalidov, Vasil and Fernandez, Pierre and Haziza, Daniel and Massa, Francisco and El-Nouby, Alaaeldin and Howes, Russell and Huang, Po-Yao and Xu, Hu and Sharma, Vasu and Li, Shang-Wen and Galuba, Wojciech and Rabbat, Mike and Assran, Mido and Ballas, Nicolas and Synnaeve, Gabriel and Misra, Ishan and Jegou, Herve and Mairal, Julien and Labatut, Patrick and Joulin, Armand and Bojanowski, Piotr},
+  journal={arXiv:2304.07193},
+  year={2023}
+}
 ```
---
+++ retico_dino/dino.py
@@ -0,0 +1,131 @@
+"""
+DINO Module
+==================
+
+This module provides extracts features from DetectedObjectsIU using DINO.
+"""
+
+
+from collections import deque
+import numpy as np
+import threading
+import time
+import torch
+from transformers import ViTFeatureExtractor, ViTModel
+
+import retico_core
+# TODO make is so that you don't need these 3 lines below
+# idealy retico-vision would be in the env so you could 
+# import it by just using:
+# from retico_vision.vision import ImageIU, DetectedObjectsIU
+import sys
+prefix = '../../'
+sys.path.append(prefix+'retico-vision')
+
+from retico_vision.vision import DetectedObjectsIU, ObjectFeaturesIU
+
+class Dinov2ObjectFeatures(retico_core.AbstractModule):
+    @staticmethod
+    def name():
+        return "DINOv2 Object Features"
+
+    @staticmethod
+    def description():
+        return "Module for extracting visual features from images."
+
+    @staticmethod
+    def input_ius():
+        return [DetectedObjectsIU]
+
+    @staticmethod
+    def output_iu():
+        return ObjectFeaturesIU
+    
+    MODELS = [
+        "facebook/dino-vits8",
+        "facebook/dino-vits16",
+        "facebook/dino-vitb8",
+        "facebook/dino-vitb16",
+    ]
+    
+    def __init__(self, model_name = "facebook/dino-vits8", show=False, **kwargs):
+        super().__init__(**kwargs)
+
+        if model_name not in self.MODELS:
+            print("Unknown model. Defaulting to facebook/dino-vits8")
+            model_name = "facebook/dino-vits8"
+
+        self.model_name = model_name
+        self.model = None
+        self.feature_extractor = None
+        self.show = show
+        self.queue = deque(maxlen=1)
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    def process_update(self, update_message):
+        for iu, ut in update_message:
+            if ut != retico_core.UpdateType.ADD:
+                continue
+            else:
+                self.queue.append(iu)
+
+    def get_clip_subimage(self, I, img_box):
+        # expected format:
+        # Numpy array, length 4, [xmin, ymin, xmax, ymax]
+
+        xmin = int(img_box[0])
+        xmax = int(img_box[2])
+        ymin = int(img_box[1])
+        ymax = int(img_box[3])
+        sub = I.crop([xmin,ymin,xmax,ymax])
+
+        if self.show:
+            import cv2
+            img_to_show = np.asarray(sub)
+            cv2.imshow('image',cv2.cvtColor(img_to_show, cv2.COLOR_RGB2BGR)) 
+            cv2.waitKey(1)
+        # pim = PImage.fromarray(sub)
+        sub.load()
+        return sub
+    
+    def _extractor_thread(self):
+        while self._extractor_thread_active:
+            if len(self.queue) == 0:
+                time.sleep(0.5)
+                continue
+
+            input_iu = self.queue.popleft()
+            image = input_iu.image
+            detected_objects = input_iu.detected_objects
+            object_features = {}
+
+            sub_img_list = []
+            for i, obj in enumerate(detected_objects):
+                sub_img = self.get_clip_subimage(image, obj)
+                sub_img_list.append(sub_img)
+
+                # img = self.preprocess(sub_img).unsqueeze(0).to(self.device)
+                # yhat = self.model.encode_image(img).cpu().numpy()
+                # object_features[i] = yhat.tolist()
+
+            inputs = self.feature_extractor(images=sub_img_list, return_tensors="pt")
+            outputs = self.model(**inputs)
+            last_hidden_states = outputs.last_hidden_state
+
+            for i, feat in enumerate(last_hidden_states):
+                print(feat.shape)
+                object_features[i] = feat
+
+            output_iu = self.create_iu(input_iu)
+            output_iu.set_object_features(image, object_features)
+            um = retico_core.UpdateMessage.from_iu(output_iu, retico_core.UpdateType.ADD)
+            self.append(um)
+
+    def prepare_run(self):
+        self.feature_extractor = ViTFeatureExtractor.from_pretrained(self.model_name)
+        self.model = ViTModel.from_pretrained(self.model_name)
+        self._extractor_thread_active = True
+        threading.Thread(target=self._extractor_thread).start()
+    
+    def shutdown(self):
+        self._extractor_thread_active = False
